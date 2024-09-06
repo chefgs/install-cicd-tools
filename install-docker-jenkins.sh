779b1a3ccd7218e2264b7d848bb3839cbac5a061
Saravanan G <g.gsaravanan@gmail.com>
2024-09-06 08:37:01 +0000

    docker install updates

--- install-docker-jenkins.sh
+++ install-docker-jenkins.sh
@@ -27,7 +27,7 @@ sudo apt-get update -y
 sudo apt update -y
 
 # Install Docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Start Docker service
 # Check if systemd is available
